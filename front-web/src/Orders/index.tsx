ad342ea22aa85eb783df57f7cd87201e004457e3
Gabriel Aguiar <gabrielaguiar1994@hotmail.com>
2021-01-09 15:58:52 -0300

    Add products by API

--- front-web/src/Orders/index.tsx
+++ front-web/src/Orders/index.tsx
@@ -1,10 +1,27 @@
+import { useEffect, useState } from 'react';
+import ProductsList from './ProductsList';
 import StepsHeader from './StepsHeader';
+import { Product } from './types';
 import './styles.css';
+import { fetchProducts } from './api';
 
 function Orders() {
+    const [products, setProducts] = useState<Product[]>([]);
+    console.log(products);
+
+    useEffect(() => {
+        fetchProducts()
+            .then(response => setProducts(response.data))
+            .catch(error => console.log(error));
+
+
+    }, []);
+
+
     return (
         <div className="orders-container">
             <StepsHeader />
+            <ProductsList products={products} />
         </div>
     )
 }
